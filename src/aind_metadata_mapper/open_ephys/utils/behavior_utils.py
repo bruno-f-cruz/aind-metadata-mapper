6823113cb3ef86a5f2f73f0751176635736c2e03
Mae Moninghoff <jcmoninghoff@gmail.com>
2024-08-28 19:25:02 -0500

    U19 ETL (#76)
    * initial commit
    
    * Update u19_etl.py
    
    * functions to ingest nanoject + begin surgery ingest
    
    * first passes running
    
    * u19 etl draft 2, only importing spec procedures
    
    * removing output zip
    
    * test initial commit
    
    * Update u19_scratch.ipynb
    
    * Create procedures.json
    
    * setting up for tests
    
    * Update pyproject.toml
    
    * tests
    
    * finalized testing
    
    * linters
    
    * Update test_u19_etl.py
    
    * linters
    
    * Update u19_etl.py
    
    * adding upgrader to dependenciest
    
    * adding utils file
    
    * Update u19_etl.py
    
    * Update pyproject.toml
    
    * linters
    
    * Update test_u19_etl.py
    
    * anonymous tests
    
    * linters
    
    * removing output
    
    * refactoring file names
    
    * linters
    
    * creates test_u19 package
    
    * linters
    
    * moving download link to jobsettings
    
    * moving utils into U19 folder
    
    * linters
    
    * fixing improper move
    
    * fixing moves
    
    * removing usage of tuples
    
    * linters
    
    * moves JobSettings to models module
    
    * linters and tests
    
    * adding JobResponses for errors
    
    * linters
    
    * testing
    
    * Update procedures.py
    
    * linters
    
    * Update procedures.py
    
    * fixing tests
    
    * trying to add status code
    
    * editing logging statements
    
    * Create __init__.py
    
    * small fixes
    
    * Update test_procedures.py
    
    * Update __init__.py
    
    * Update test_procedures.py
    
    * Update __init__.py
    
    * new folder organization
    
    * paths
    
    * fixing files
    
    * moving files
    
    * moving files
    
    * fixing paths
    
    * Update procedures.py
    
    * Update procedures.py
    
    * adds u19 dep
    
    * adds u19 into [all]
    
    ---------
    
    Co-authored-by: Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>

--- src/aind_metadata_mapper/open_ephys/utils/behavior_utils.py
+++ src/aind_metadata_mapper/open_ephys/utils/behavior_utils.py
@@ -730,9 +730,9 @@ def fix_omitted_end_frame(stim_pres_table: pd.DataFrame) -> pd.DataFrame:
         stim_pres_table[stim_pres_table["omitted"]]["start_frame"]
         + median_stim_frame_duration
     )
-    stim_pres_table.loc[
-        stim_pres_table["omitted"], "end_frame"
-    ] = omitted_end_frames
+    stim_pres_table.loc[stim_pres_table["omitted"], "end_frame"] = (
+        omitted_end_frames
+    )
 
     stim_dtypes = stim_pres_table.dtypes.to_dict()
     stim_dtypes["start_frame"] = int
@@ -796,9 +796,9 @@ def compute_is_sham_change(
                 if np.array_equal(
                     active_images, stim_image_names[passive_block_mask].values
                 ):
-                    stim_df.loc[
-                        passive_block_mask, "is_sham_change"
-                    ] = stim_df[active_block_mask]["is_sham_change"].values
+                    stim_df.loc[passive_block_mask, "is_sham_change"] = (
+                        stim_df[active_block_mask]["is_sham_change"].values
+                    )
 
     return stim_df.sort_index()
 
